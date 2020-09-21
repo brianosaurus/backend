52e0e01c2768a7a79b0d94cd40fec23d664f6f41
Brian Woods <me@brian.biz>
2020-09-20 17:49:36 -0700

    saving stuff

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/TutorialEdge/realtime-chat-go-react
+
+go 1.15
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	golang.org/x/tools v0.0.0-20200918232735-d647fc253266 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/brianosaurus/backend/pkg/websocket"
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+\/\n", err)
+	}
+
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+
+	http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+	fmt.Println("foo bar foo")
+
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
